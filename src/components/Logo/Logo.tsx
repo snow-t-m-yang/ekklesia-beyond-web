f8f2da53670766e0fed6cd7d79eff80565c90013
Snow Yáng <ytm199891@gmail.com>
2025-01-11 12:01:02 -0600

    Change logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,14 +16,14 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
-      width={193}
+      alt="Ekklesia Beyond Logo"
+      width={120}
       height={34}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      className={clsx('', className)}
+      src="/beyond-logo.webp"
     />
   )
 }
