18640ec549ee589feb99f9d2ae8a17a082bfb772
Snow Yáng <ytm199891@gmail.com>
2025-01-11 13:21:33 -0600

    Remove logo invert

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -33,7 +33,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="bg-black/50 w-full px-3 md:px-7 py-3 fixed backdrop-blur-xl z-20" {...(theme ? { 'data-theme': theme } : {})}>
       <div className="flex justify-between items-center">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high" />
         </Link>
         <HeaderNav data={data} />
       </div>
