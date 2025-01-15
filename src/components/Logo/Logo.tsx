39ad8dde592a942e5616ab00e63005cd94db3cb4
Snow Yáng <ytm199891@gmail.com>
2025-01-14 18:43:47 -0600

    Add responsive logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,29 +1,35 @@
-import clsx from 'clsx'
-import React from 'react'
-import Image from 'next/image'
+import clsx from 'clsx';
+import React from 'react';
+import Image from 'next/image';
 
 interface Props {
-  className?: string
-  loading?: 'lazy' | 'eager'
-  priority?: 'auto' | 'high' | 'low'
+  className?: string;
+  loading?: 'lazy' | 'eager';
+  priority?: 'auto' | 'high' | 'low';
 }
 
 export const Logo = (props: Props) => {
-  const { loading: loadingFromProps, priority: priorityFromProps, className } = props
+  const { loading: loadingFromProps, priority: priorityFromProps, className } = props;
 
-  const loading = loadingFromProps || 'lazy'
-  const priority = priorityFromProps || 'low'
+  const loading = loadingFromProps || 'lazy';
+  const priority = priorityFromProps || 'low';
 
   return (
-    <Image
-      alt="Ekklesia Beyond Logo"
-      width={110}
-      height={34}
-      loading={loading}
-      fetchPriority={priority}
-      decoding="async"
-      className={clsx('', className)}
-      src="/beyond-logo.webp"
-    />
-  )
-}
+    <div
+      className={clsx(
+        'sm:w-[10rem] sm:h-[5rem] xl:w-[12rem] xl:h-[6rem] w-[5rem] h-[3rem] relative transition-all duration-150',
+        className,
+      )}
+    >
+      <Image
+        alt="Ekklesia Beyond Logo"
+        fill
+        loading={loading}
+        fetchPriority={priority}
+        decoding="async"
+        className="object-contain"
+        src="/beyond-logo.webp"
+      />
+    </div>
+  );
+};
