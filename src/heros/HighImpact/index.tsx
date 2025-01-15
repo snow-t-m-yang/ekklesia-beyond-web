1790a7c77246ce6a69ce01251a023b4419faade7
Snow Yáng <ytm199891@gmail.com>
2025-01-14 21:02:24 -0600

    Fix h1 overflow

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -1,20 +1,20 @@
-'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
-import React, { useEffect } from 'react'
+'use client';
+import { useHeaderTheme } from '@/providers/HeaderTheme';
+import React, { useEffect } from 'react';
 
-import type { Page } from '@/payload-types'
+import type { Page } from '@/payload-types';
 
-import { CMSLink } from '@/components/Link'
-import { Media } from '@/components/Media'
-import RichText from '@/components/RichText'
-import { AuroraBackground } from '@/components/ui/aurora-background'
+import { CMSLink } from '@/components/Link';
+import { Media } from '@/components/Media';
+import RichText from '@/components/RichText';
+import { AuroraBackground } from '@/components/ui/aurora-background';
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
-  const { setHeaderTheme } = useHeaderTheme()
+  const { setHeaderTheme } = useHeaderTheme();
 
   useEffect(() => {
-    setHeaderTheme('light')
-  })
+    setHeaderTheme('light');
+  });
 
   return (
     <>
@@ -30,7 +30,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
                       <li key={i}>
                         <CMSLink {...link} />
                       </li>
-                    )
+                    );
                   })}
                 </ul>
               )}
@@ -46,8 +46,14 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
         <AuroraBackground>
           <div className="flex items-center justify-center" data-theme="light">
             <div className="container pt-16 mb-8 z-10 relative flex items-center justify-center">
-              <div className="max-w-[36.5rem] md:text-center">
-                {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+              <div className="max-w-[36.5rem]  md:text-center">
+                {richText && (
+                  <RichText
+                    className="mb-6 line-clamp-[7] sm:line-clamp-none"
+                    data={richText}
+                    enableGutter={false}
+                  />
+                )}
                 {Array.isArray(links) && links.length > 0 && (
                   <ul className="flex md:justify-center gap-4">
                     {links.map(({ link }, i) => {
@@ -55,7 +61,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
                         <li key={i}>
                           <CMSLink {...link} />
                         </li>
-                      )
+                      );
                     })}
                   </ul>
                 )}
@@ -65,5 +71,5 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
         </AuroraBackground>
       )}
     </>
-  )
-}
+  );
+};
