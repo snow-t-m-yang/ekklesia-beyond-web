7fa8a8da635c21c4736c3f6bb16c4143af232e55
Snow Yáng <ytm199891@gmail.com>
2025-01-16 14:48:48 -0600

    Add rights reserves

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -1,34 +1,38 @@
-import { getCachedGlobal } from '@/utilities/getGlobals'
-import Link from 'next/link'
-import React from 'react'
+import { getCachedGlobal } from '@/utilities/getGlobals';
+import Link from 'next/link';
+import React from 'react';
 
-import type { Footer } from '@/payload-types'
+import type { Footer } from '@/payload-types';
 
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
-import { CMSLink } from '@/components/Link'
-import { Logo } from '@/components/Logo/Logo'
+import { ThemeSelector } from '@/providers/Theme/ThemeSelector';
+import { CMSLink } from '@/components/Link';
+import { Logo } from '@/components/Logo/Logo';
 
 export async function Footer() {
-  const footerData: Footer = await getCachedGlobal('footer', 1)()
+  const footerData: Footer = await getCachedGlobal('footer', 1)();
 
-  const navItems = footerData?.navItems || []
+  const navItems = footerData?.navItems || [];
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="mt-auto border-t border-border bg-white dark:bg-card">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
+      <section className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <Link className="flex items-center" href="/">
           <Logo />
         </Link>
-
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
           <ThemeSelector />
           <nav className="flex flex-col md:flex-row gap-4">
             {navItems.map(({ link }, i) => {
-              return <CMSLink key={i} {...link} />
+              return <CMSLink key={i} {...link} />;
             })}
           </nav>
         </div>
-      </div>
+      </section>
+
+      <section className="container pb-8 text-muted-foreground">
+        <span>{`© ${currentYear} Ekklesia Beyond All rights reserved.`}</span>
+      </section>
     </footer>
-  )
+  );
 }
